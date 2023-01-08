f52d7b48d7314cc5c1737875dabc8c74e76cb495
Erik Auerswald <auerswal@unix-ag.uni-kl.de>
2023-01-08 03:59:37 +0100

    update copyright years to include 2023

--- section.go
+++ section.go
@@ -1,6 +1,6 @@
 /*
    section - print sections of a text file matching a pattern
-   Copyright (C) 2019-2022  Erik Auerswald <auerswal@unix-ag.uni-kl.de>
+   Copyright (C) 2019-2023  Erik Auerswald <auerswal@unix-ag.uni-kl.de>
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
