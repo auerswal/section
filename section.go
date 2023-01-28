8bcdf79c7a136ce5de802cc747918f84741a8833
Erik Auerswald <auerswal@unix-ag.uni-kl.de>
2023-01-28 10:53:40 +0100

    call this version 0.5.1 and update NEWS

--- section.go
+++ section.go
@@ -33,7 +33,7 @@ import (
 const (
 	// program information
 	PROG    = "section"
-	VERSION = "0.5.0"
+	VERSION = "0.5.1"
 	// technical peculiarities
 	ARB_BUF_LIM = 512 * 1024 * 1024 // 512MiB
 	// internal regular expressions
