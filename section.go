673f6de71076076bc10f9485db8f057296e3d3e6
Erik Auerswald <auerswal@unix-ag.uni-kl.de>
2023-05-19 19:45:32 +0200

    mark version as WIP by appending a '+'

--- section.go
+++ section.go
@@ -33,7 +33,7 @@ import (
 const (
 	// program information
 	PROG    = "section"
-	VERSION = "0.7.0"
+	VERSION = "0.7.0+"
 	// technical peculiarities
 	ARB_BUF_LIM = 512 * 1024 * 1024 // 512MiB
 	// internal regular expressions
