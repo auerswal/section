42ba443a98a40a06f4bd505157d991bb0b3078a1
Erik Auerswald <auerswal@unix-ag.uni-kl.de>
2023-05-20 17:09:48 +0200

    generate_man_page_date.sh: adjust structure
    This should not change the semantics of man page date generation,
    but should make the process simpler to understand and modify.

--- generate_man_page_date.sh
+++ generate_man_page_date.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # generate_man_page_date.sh
-# Copyright (C) 2021  Erik Auerswald <auerswal@unix-ag.uni-kl.de>
+# Copyright (C) 2021-2023  Erik Auerswald <auerswal@unix-ag.uni-kl.de>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,11 +16,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# when in a git repository, use modification date of man page source
 if type 'git' >/dev/null 2>&1 && test -d '.git'; then
   git log -n1 --date=short 'section.1.in' | awk '/^Date:/ { print $2 }'
   exit
-elif test -f 'section.1' && test -r 'section.1'; then
-  awk 'NR == 1 { gsub(/\"/, "", $4); print $4; exit }' 'section.1'
-else
-  date -r 'section.1.in' +%Y-%m-%d
 fi
+# when a man page is available, keep its date
+if test -f 'section.1' && test -r 'section.1'; then
+  awk 'NR == 1 { gsub(/\"/, "", $4); print $4; exit }' 'section.1'
+  exit
+fi
+# as a last resort, use the file modification date of man page source
+date -r 'section.1.in' +%Y-%m-%d
